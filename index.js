9623413697149b6c6c678c505774629049137b7c
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-02-28 13:13:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var AWS = require('aws-sdk');
 var phantomjs = require('phantomjs-prebuilt');
+var fs = require('fs');
 
 exports.handler = function(event, context, callback) {
     console.log('I am on master');
@@ -8,6 +9,14 @@ exports.handler = function(event, context, callback) {
     const LAMBDA_TASK_ROOT = process.env.LAMBDA_TASK_ROOT;
     console.log('LAMBDA_TASK_ROOT=' + LAMBDA_TASK_ROOT);
     
+    fs.readdir(LAMBDA_TASK_ROOT, function(err, items) {
+        console.log(items);
+
+        for (var i=0; i<items.length; i++) {
+            console.log(items[i]);
+        }
+    });
+
     var phantom = phantomjs.exec('phantomjs-script.js', LAMBDA_TASK_ROOT, 'arg2');
 
     phantom.stdout.on('data', function(buf) {
