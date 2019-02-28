7148eaa5273bf5651e2db38a56954e3294d9bf3c
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-02-28 10:50:25 +0000

    Update phantomjs-script.js

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -4,8 +4,35 @@ var webPage = require('webpage');
 var page = webPage.create();
 
 page.viewportSize = {
-	width: 640,
-	height: 480
+	width: 1024,
+	height: 768
+};
+
+phantom.onError = function(msg, trace) {
+  var msgStack = ['PHANTOM ERROR: ' + msg];
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+    });
+  }
+  console.log(msgStack.join('\n'));
+  phantom.exit(1);
+};
+
+page.onError = function(msg, trace) {
+
+  var msgStack = ['ERROR: ' + msg];
+
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+    });
+  }
+
+  console.error(msgStack.join('\n'));
+
 };
 
 console.log('opening page...');
@@ -14,7 +41,20 @@ page.open('http://amchartstestserver-env.e6gtdmyuck.eu-west-2.elasticbeanstalk.c
 
   console.log('page opened, content=' + page.content);
 	
-  var base64 = page.renderBase64('PNG');
-  console.log('base64=' + base64);
+  try {	
+	
+    console.log('rendering base64...');
+	  
+    var base64 = page.renderBase64('PNG');
+    console.log('base64=' + base64);
+  
+  } catch(err) {
+	  
+    console.log('error');
+    console.log('err.message=' + err.message);  
+  }
+	
+  console.log('exiting phantom script');
+	
   phantom.exit();
 });
