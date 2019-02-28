0d5a56d45ee1a22dbc0ca7b4e0ef303946ba9ccb
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-02-28 16:12:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,38 +4,33 @@ var phantomjs = require('phantomjs-prebuilt');
 var fs = require('fs');
 
 exports.handler = function(event, context, callback) {
-    console.log('I am on master');
     
-    var LAMBDA_TASK_ROOT = process.env.LAMBDA_TASK_ROOT;
-    if (LAMBDA_TASK_ROOT == null) {
-        LAMBDA_TASK_ROOT = '/';
-    } else {
-        LAMBDA_TASK_ROOT = LAMBDA_TASK_ROOT + '/';
-    }
-    console.log('LAMBDA_TASK_ROOT=' + LAMBDA_TASK_ROOT);
+    console.log('started');
     
-var base64 = '';
+    // TODO get chart html file name from function parameter?
+    var chartHtmlFile = process.env.LAMBDA_TASK_ROOT + '/' + 'chart.html';
+    console.log('chartHtmlFile: ' + chartHtmlFile);
+    
+    var chartImageBase64 = '';
 
-    var phantom = phantomjs.exec('phantomjs-script.js', LAMBDA_TASK_ROOT, 'arg2');
+    var phantom = phantomjs.exec('phantomjs-script.js', chartHtmlFile, 'otherArg');
 
     phantom.stdout.on('data', function(buf) {
-       
-        var content = String(buf);
-        
-        base64 += content;
-        
-        console.log('[STR] stdout "%s"', content);
+        var base64Data = String(buf);
+        console.log('got base64 data: ' + base64Data);
+        chartImageBase64 += base64Data;
     });
+    
     phantom.stderr.on('data', function(buf) {
-        console.log('[STR] stderr "%s"', String(buf));
+        console.log('stderr "%s"', String(buf));
     });
     phantom.on('close', function(code) {
-        console.log('[END] code', code);
+        console.log('code', code);
     });
 
     phantom.on('exit', code => {
         
-        console.log('entered phantom.onExit, code=' + code);
+        console.log('phantomjs exit, code: ' + code);
         
         const response = {
             statusCode: 200,
@@ -43,8 +38,9 @@ var base64 = '';
             body: base64,
             isBase64Encoded : true,
         };
+        
+        // TODO error response
 
         callback(null, response);
-        
    });
 };
