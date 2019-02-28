73515bf0405d61a4cedb5b02cce1ba9457e10cba
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-02-28 08:36:14 +0000

    Update phantomjs-script.js
    Changed to base64 rendering

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,22 +1,32 @@
 console.log('start phantom');
 
 var page = require('webpage').create();
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
 
-console.log('opening page...');
+var url = 'https://www.google.co.uk/';
 
-page.open('https://s.codepen.io/amcharts/debug/cd2e8ce27e3a96f43bb79d5d23722d11');
+console.log('url=' + url);
 
-console.log('after page.open');
+page.open(url, function start(status) {
 
-console.log('calling page.onLoadFinished');
-
-page.onLoadFinished = function(status) {
-  var url = page.url;
-  console.log("Status:  " + status);
-  console.log("Loaded:  " + url);
-  page.render("/tmp/example.png");
+  console.log('after page.open');
   
-  console.log('image saved');
-  
-  phantom.exit();
-};
+  // Set the background to white, just in case
+  page.evaluate(function() {
+    document.body.style.background = 'white';
+  }); 
+
+  // Wait an extra second for things to load
+  window.setTimeout(function() { 
+
+    console.log('rendering base64');
+    
+    // Return image of the page as base64-encoded string
+    var base64 = page.renderBase64('JPEG');
+    
+    console.log('base64=' + base64);
+    
+    phantom.exit();
+
+  }, 1000);
+});
