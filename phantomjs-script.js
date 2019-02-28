c7cd032d68059cde8f7292ba7c718bc8a95435cb
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-02-28 09:09:36 +0000

    Update phantomjs-script.js
    added page error logging

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -8,10 +8,35 @@ page.viewportSize = {
 	height: 1080
 };
 
+page.onError = function(msg, trace) {
+
+  var msgStack = ['ERROR: ' + msg];
+
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+    });
+  }
+
+  console.error(msgStack.join('\n'));
+
+};
+
+page.onResourceTimeout = function(e) {
+  console.log('timeout, errorCode=' + e.errorCode);   // it'll probably be 408 
+  console.log('timeout, errorString=' + e.errorString); // it'll probably be 'Network timeout on resource'
+  console.log('timeout, url=' + e.url);         // the url whose request timed out
+  phantom.exit(1);
+};
+
+
 console.log('opening page...');
 
 page.open('http://s.codepen.io/amcharts/debug/cd2e8ce27e3a96f43bb79d5d23722d11', function (status) {
   
+  console.log('page opened, content=' + page.content);
+	
   console.log('page opened, rendering base64...');
   
   var base64 = page.renderBase64('PNG');
