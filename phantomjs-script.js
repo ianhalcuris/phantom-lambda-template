a0f18dbc7e23f82df69a3313cef4a8b4e15d07e9
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-02-28 08:03:56 +0000

    Update phantomjs-script.js
    Added amchart URL and save as png

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,10 +1,13 @@
 console.log('start phantom');
 
 var page = require('webpage').create();
-page.open('https://www.google.co.uk/', function(status) {
+page.open('https://s.codepen.io/amcharts/debug/cd2e8ce27e3a96f43bb79d5d23722d11', function(status) {
   var title = page.evaluate(function() {
     return document.title;
   });
   console.log('Page title is ' + title);
+  
+  page.render('example.png');
+  
   phantom.exit();
 });
