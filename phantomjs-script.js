fab184f3ac054bb130151b0b99f6bcf2e6aa34a0
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-02-28 08:50:18 +0000

    Update phantomjs-script.js
    Changed to phantomjs tutorial base64 render code

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,31 +1,23 @@
-console.log('start phantom');
+console.log('started phantom');
 
-var page = require('webpage').create();
-page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
+var webPage = require('webpage');
+var page = webPage.create();
 
-var url = 'https://www.google.co.uk/';
-
-console.log('url=' + url);
-
-page.open(url);
-
-console.log('after page.open');
-
-page.onLoadFinished = function(status) {
-  console.log('entered page.onLoadFinished, status=' + status);
-  // Do other things here...
-  
-  console.log('rendering base64');
-    
-  // Return image of the page as base64-encoded string
-  //var base64 = page.renderBase64('JPEG');
-    
-  //console.log('base64=' + base64);
-  
-  console.log('all done, calling phantom.exit');
-  
-  phantom.exit();
-  
-  console.log('after phantom.exit');
-  
+page.viewportSize = {
+	width: 1920,
+	height: 1080
 };
+
+console.log('opening page...');
+
+page.open('http://phantomjs.org', function (status) {
+  
+  console.log('page opened, rendering base64...');
+  
+  var base64 = page.renderBase64('PNG');
+  
+  console.log('base64 rendered');
+  
+  console.log(base64);
+  phantom.exit();
+});
