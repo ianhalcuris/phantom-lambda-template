3e301f0ff149a08034f2a0b3c00a50d792dcc24e
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-01 11:33:08 +0000

    Update phantomjs-script.js

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -8,13 +8,17 @@ var apiData = args[2];
 // TODO pass in as args depending on chart type?
 page.viewportSize = { width: 1920, height: 1080 };
 
+page.onConsoleMessage = function(msg) {
+  console.log('console msg: ' + msg);
+}
+
 page.open(chartHtmlFile, function (status) {
 	
-	console.log('in phamtom-script, apiData=' + apiData);
+	// apiData is defined here
 	
 	page.evaluate(function(apiData) {
 		
-		console.log('page.evaluate, apiData=' + apiData);
+		console.log('in page.evaluate, apiData=' + apiData);
 		
     		renderChart(apiData);
   	});
