3c856946d5c0939b41c9358c200b909540cad290
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-01 10:07:35 +0000

    Update phantomjs-script.js

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -3,11 +3,12 @@ var args = require('system').args;
 var webPage = require('webpage');
 var page = webPage.create();
 var chartHtmlFile = args[1];
+var apiData = args[2];
 
 // TODO pass in as args depending on chart type?
 page.viewportSize = { width: 1920, height: 1080 };
 
-page.open(chartHtmlFile, function (status) {
+page.open(chartHtmlFile + '?apiData=' + apiData, function (status) {
     setTimeout(function() {
 	    
         var base64 = page.renderBase64('PNG');
