b76ab2fe42f3190db18941f5c5f07cf87e333306
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-01 11:47:08 +0000

    Update phantomjs-script.js

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,36 +1,28 @@
 
 var args = require('system').args;
 var webPage = require('webpage');
+
 var page = webPage.create();
-var chartHtmlFile = args[1];
+
+var htmlFile = args[1];
 var apiData = args[2];
 
 // TODO pass in as args depending on chart type?
 page.viewportSize = { width: 1920, height: 1080 };
 
-page.onConsoleMessage = function(msg) {
-  console.log('console msg: ' + msg);
-}
-
-page.open(chartHtmlFile, function (status) {
-	
-	// apiData is defined here
+page.open(htmlFile, function (status) {
 	
 	page.evaluate(function(apiData) {
-		
-		// apiData is NOT defined here
-		
-		console.log('in page.evaluate, apiData=' + apiData);
-		
     		renderChart(apiData);
   	}, apiData);
 	
     	setTimeout(function() {
 		
-        	var base64 = page.renderBase64('PNG');
-        	console.log(base64);
+        	console.log(
+			page.renderBase64('PNG')
+		);
 	    
         	phantom.exit();
 	        
-    	}, 5000); // TODO tweak this timeout 
+    	}, 5000); // TODO optimise this timeout 
 });
