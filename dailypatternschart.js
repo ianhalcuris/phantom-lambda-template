4469518379e3f99df813d71aa96d44b97c93aae4
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-04 10:04:38 +0000

    Update dailypatternschart.js

--- dailypatternschart.js
+++ dailypatternschart.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 var request = require('request');
 const util = require('util');
 
-function getApiData(hubId) {
 
-	var apiUrl = util.format(process.env.API_URL, hubId);
-	console.log('apiUrl: ' + apiUrl);
+function getApiData(apiUrl) {
 	
+	console.log('calling api, url: ' + apiUrl);
+
 	var options = {
 		url: apiUrl,
 		headers: {
@@ -28,9 +28,91 @@ function getApiData(hubId) {
 }
 
 exports.handler = function(event, context, callback) {
+	
+	console.log('started');
     
-    	console.log('started');
-    	console.log('getting API data...');
+	var hubId = event.hubId;
+	
+    	console.log('hubId: ' + hubId);
+	
+	var summaryDataApiUrl = util.format(process.env.API_HOST + '/memo/service/hub/%s/summary', hubId);
+
+	console.log('getting summary data...');
+	
+	// Get summary data
+	getApiData(summaryDataApiUrl).then(function(summaryData) {
+
+		console.log('got summary data: ' + summaryData);
+		
+		// Get discrete data
+		var discreteDataApiUrl = util.format(process.env.API_HOST + '/memo/service/hub/%s/service/discreteData', hubId);
+		
+		getApiData(discreteDataApiUrl).then(function(discreteData) {
+			
+			console.log('got discrete data: ' + discreteData);
+			
+			const response = {
+				statusCode: 200
+			};
+			
+			callback(null, response);
+			
+		}, function(err) {
+		
+			console.log('IAN-TRACE ERROR ~ ' + err);
+			// TODO error function response
+		});
+/*
+		// TODO get chart html file name from function parameter?
+		var chartTemplate = process.env.LAMBDA_TASK_ROOT + '/' + process.env.CHART_TEMPLATE;
+		console.log('chartTemplate: ' + chartTemplate);
+
+		var chartImageBase64 = '';
+	
+		
+		var phantom = phantomjs.exec('phantomjs-script.js', chartTemplate, result);
+
+	    	phantom.stdout.on('data', function(buf) {
+			var base64Data = String(buf).replace(/\n$/, '');
+			console.log('got base64 data: ' + base64Data);
+			chartImageBase64 += base64Data;
+	    	});
+
+	    	phantom.stderr.on('data', function(buf) {
+			console.log('stderr "%s"', String(buf));
+	    	});
+	    	phantom.on('close', function(code) {
+			console.log('code', code);
+	    	});
+
+	    	phantom.on('exit', code => {
+
+			console.log('phantomjs exit, code: ' + code);
+
+			const response = {
+				statusCode: 200,
+				headers: {'Content-type' : 'image/png'},
+				body: chartImageBase64,
+				isBase64Encoded : true,
+			};
+
+			// TODO error response
+
+			callback(null, response);
+		});
+		
+	}, function(err) {
+		
+		console.log('IAN-TRACE ERROR ~ ' + err);
+		// TODO error function response
+    	});
+	
+	
+	
+	
+	
+	
+	
     
 	// Get API data
 	getApiData(event.hubId).then(function(result) {
@@ -80,4 +162,5 @@ exports.handler = function(event, context, callback) {
 		console.log('IAN-TRACE ERROR ~ ' + err);
 		// TODO error function response
     	});
+*/
 };
