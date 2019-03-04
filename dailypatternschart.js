6c65767d864351185d7be87622cbe5a3d75fd1be
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-04 16:47:41 +0000

    Update dailypatternschart.js

--- dailypatternschart.js
+++ dailypatternschart.js
@@ -6,14 +6,12 @@ var chartProcessor = require('./chartprocessor');
 exports.handler = function(event, context, callback) {
 
 	console.log('started');
-	
-	const hubId = event.hubId;
-	
-	const apiUrl = util.format(process.env.API_HOST + '/memo/service/insight/hub/%s/service/discreteData?precision=5', hubId);
+		
+	const apiUrl = util.format(process.env.API_HOST + '/memo/service/insight/hub/%s/service/discreteData?precision=5', event.hubId);
 	const chartTemplate = process.env.LAMBDA_TASK_ROOT + '/dailypatternschart.html';
 	
 	console.log('apiUrl: ' + apiUrl);
 	console.log('chartTemplate: ' + chartTemplate);
 	
-	//chartProcessor.renderChart(apiUrl, chartTemplate, context, callback);
+	chartProcessor.renderChart(apiUrl, chartTemplate, context, callback);
 };
