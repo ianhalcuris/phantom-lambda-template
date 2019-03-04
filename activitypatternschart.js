1a34ffcb4c7127870042d6820d5bb0c6cf33ff04
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-04 16:50:12 +0000

    Create activitypatternschart.js

---
+++ activitypatternschart.js
@@ -0,0 +1,16 @@
+var AWS = require('aws-sdk');
+var util = require('util');
+var chartProcessor = require('./chartprocessor');
+
+exports.handler = function(event, context, callback) {
+
+	console.log('started');
+		
+	const apiUrl = util.format(process.env.API_HOST + '/memo/service/insight/hub/%s/service/discreteData?precision=5', event.hubId);
+	const chartTemplate = process.env.LAMBDA_TASK_ROOT + '/dailypatternschart.html';
+	
+	console.log('apiUrl: ' + apiUrl);
+	console.log('chartTemplate: ' + chartTemplate);
+	
+	chartProcessor.renderChart(apiUrl, chartTemplate, context, callback);
+};
