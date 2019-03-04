8f0e6e21d3a5152b4442443681c4c3e3c602b124
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-04 11:20:34 +0000

    Update dailypatternschart.js

--- dailypatternschart.js
+++ dailypatternschart.js
@@ -54,8 +54,8 @@ exports.handler = function(event, context, callback) {
 			service.devices.forEach(device => {
 				device.data.forEach(data => {
 					chartData.push({
-						start: new String(new Date(data.start).getTime() / 1000),
-						end: new String(new Date(data.end).getTime() / 1000),
+						start: new String(new Date(data.start * 1000).getTime()),
+						end: new String(new Date(data.end * 1000).getTime()),
 						category: device.name
 				    	})
 				});
