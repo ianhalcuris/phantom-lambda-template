258fd8e26f0dd49a3bb9d691bb6c273213838712
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-04 16:35:32 +0000

    Update dailypatternschart.js

--- dailypatternschart.js
+++ dailypatternschart.js
@@ -1,92 +1,17 @@
-var AWS = require('aws-sdk');
-var phantomjs = require('phantomjs-prebuilt');
-var fs = require('fs');
-var request = require('request');
+const AWS = require('aws-sdk');
 const util = require('util');
+const chartProcessor = require('./chartprocessor');
 
-var myTest = require('./mytest');
-
-function getApiData(apiUrl) {
-	
-	console.log('calling api, url: ' + apiUrl);
-
-	var options = {
-		url: apiUrl,
-		headers: {
-		  'MEMO-USER-ID': '10'
-		}
-	};
-
- 	return new Promise(function(resolve, reject) {
-		request.get(options, function(err, resp, body) {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(body);
-			}
-		})
-	})
-}
 
 exports.handler = function(event, context, callback) {
 
 	console.log('started');
-    
-	var hubId = event.hubId;
 	
-    	console.log('hubId: ' + hubId);
-
-	console.log('getting API data...');
-
-	var apiUrl = util.format(process.env.API_HOST + '/memo/service/insight/hub/%s/service/discreteData?precision=5', hubId);
-
-	myTest.renderChart(apiUrl, context, callback);
+	const apiUrl = util.format(process.env.API_HOST + '/memo/service/insight/hub/%s/service/discreteData?precision=5', hubId);
+	const chartTemplate = process.env.LAMBDA_TASK_ROOT + '/dailypatternschart.html';
 	
+	console.log('apiUrl: ' + apiUrl);
+	console.log('chartTemplate: ' + chartTemplate);
 	
-	getApiData(apiUrl).then(function(result) {
-
-		console.log('got API data: ' + result);
-			
-		var chartTemplate = process.env.LAMBDA_TASK_ROOT + '/dailypatternschart.html';
-		console.log('chart template: ' + chartTemplate);
-
-		var chartImageBase64 = '';
-
-		var phantom = phantomjs.exec('phantomjs-script.js', chartTemplate, result);
-
-	    	phantom.stdout.on('data', function(buf) {
-			var base64Data = String(buf).replace(/\n$/, '');
-			console.log('got base64 data: ' + base64Data);
-			chartImageBase64 += base64Data;
-	    	});
-
-	    	phantom.stderr.on('data', function(buf) {
-			console.log('stderr "%s"', String(buf));
-	    	});
-	    	phantom.on('close', function(code) {
-			console.log('code', code);
-	    	});
-
-	    	phantom.on('exit', code => {
-
-			console.log('phantomjs exit, code: ' + code);
-
-			const response = {
-				statusCode: 200,
-				headers: {'Content-type' : 'image/png'},
-				body: chartImageBase64,
-				isBase64Encoded : true,
-			};
-
-			// TODO error response
-
-			callback(null, response);
-		});
-	
-	}, function(err) {
-
-		console.log('error getting discrete data: ' + err);
-		// TODO error function response
-	});
-
+	chartProcessor.handler(apiUrl, chartTemplate, context, callback);
 };
