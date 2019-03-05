33e51a6e77c220cbaf330f96ade66d204780010a
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-05 10:58:37 +0000

    Create bathroompatternschart.js

---
+++ bathroompatternschart.js
@@ -0,0 +1,16 @@
+var AWS = require('aws-sdk');
+var util = require('util');
+var chartProcessor = require('./chartprocessor');
+
+exports.handler = function(event, context, callback) {
+
+	console.log('started');
+		
+	const apiUrl = util.format('https://' + process.env.API_HOSTNAME + '/memo/service/insight/hub/%s/service/discreteData?precision=5', event.hubId);
+	const chartTemplate = process.env.LAMBDA_TASK_ROOT + '/charts/dailypatterns.html';
+	
+	console.log('apiUrl: ' + apiUrl);
+	console.log('chartTemplate: ' + chartTemplate);
+	
+	chartProcessor.renderChart(apiUrl, chartTemplate, context, callback);
+};
