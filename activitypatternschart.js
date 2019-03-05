8d57ee956c92795e63eda192d8c240a778c03512
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-05 09:10:53 +0000

    Update activitypatternschart.js

--- activitypatternschart.js
+++ activitypatternschart.js
@@ -6,7 +6,7 @@ exports.handler = function(event, context, callback) {
 
 	console.log('started');
 		
-	const apiUrl = util.format(process.env.API_HOST + '/memo/service/insight/hub/%s/service/scatterData?offset=13&range=14&precision=5', event.hubId);
+	const apiUrl = util.format('https://' + process.env.API_HOSTNAME + '/memo/service/insight/hub/%s/service/scatterData?offset=13&range=14&precision=5', event.hubId);
 	const chartTemplate = process.env.LAMBDA_TASK_ROOT + '/charts/activitypatterns.html';
 	
 	console.log('apiUrl: ' + apiUrl);
