ae272c57c345d4fc85c9c70e3fa7d9f6d8cde9a7
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-08 11:55:52 +0000

    Update phantomjs-script.js

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -8,12 +8,21 @@ var htmlFile = args[1];
 var apiData = args[2];
 
 page.onCallback = function(data) {
-  console.log('CALLBACK: ' + JSON.stringify(data));
-  // Prints 'CALLBACK: { "hello": "world" }'
-};
 
-page.onConsoleMessage = function(msg, lineNum, sourceId) {
-  console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+	var chartdiv = page.evaluate(function () {
+		return document.getElementById('chartdiv').getBoundingClientRect();
+	});
+	page.clipRect = {
+		top: chartdiv.top,
+		left: chartdiv.left,
+		width: chartdiv.width,
+		height: chartdiv.height
+	};
+	console.log(
+		page.renderBase64('PNG')
+	);
+
+	phantom.exit();
 };
 
 page.open(htmlFile, function (status) {
