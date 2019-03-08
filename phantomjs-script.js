af50a0993bc4756d6e6e86e585ddaf51400fa44c
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-03-08 12:10:58 +0000

    Update phantomjs-script.js

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -32,26 +32,4 @@ page.open(htmlFile, function (status) {
     		renderChart(apiData);
 		
   	}, apiData);
-/*	
-    	setTimeout(function() {
-
-		var chartdiv = page.evaluate(function () {
-            		return document.getElementById('chartdiv').getBoundingClientRect();
-        	});
-
-		page.clipRect = {
-			top: chartdiv.top,
-			left: chartdiv.left,
-		    	width: chartdiv.width,
-		    	height: chartdiv.height
-		};
-
-        	console.log(
-			page.renderBase64('PNG')
-		);
-	    
-        	phantom.exit();
-	        
-    	}, 20000); // TODO optimise this timeout 
-*/
 });
