c3772b9838df68be74b01912d860c93f91e06054
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-07-29 14:19:08 +0100

    Update chartprocessor.js

--- chartprocessor.js
+++ chartprocessor.js
@@ -5,6 +5,8 @@ var request = require('request');
 const util = require('util');
 var uuid = require('uuid');
 
+const TMP_DIR = '../../tmp/';
+
 function getApiData(apiUrl) {
 	
 	console.log('calling api: ' + apiUrl);
@@ -29,46 +31,31 @@ function getApiData(apiUrl) {
 
 exports.renderChart = function(apiUrl, chartTemplate, context, callback) {	
 	
-	console.log('entered chart processor');
-	
-	console.log('chartprocessor apiUrl:' + apiUrl);
-	console.log('chartprocessor chartTemplate:' + chartTemplate);
+	console.log('[chart-processor] apiUrl:' + apiUrl);
+	console.log('[chart-processor] chartTemplate:' + chartTemplate);
 	
 	getApiData(apiUrl).then(function(data) {
 
-		console.log('IAN-TRACE [chartprocessor] - got data: ' + data);
-		console.log('IAN-TRACE [chartprocessor] - data.length = ' + data.length);
-				
+		console.log('[chart-processor] - data: ' + data);
+		console.log('[chart-processor] - data.length: ' + data.length);
+		
+
+		var dataFile = TMP_DIR + uuid.v4() + '.json';
 		var chartImageBase64 = '';
 		
-		/*
-		    Can't pass the data in here because it may be too large Need 
-		    to either [1] get the data in the phantom process or [2] save 
-		    the data to file and load it in the phantom process.
-		*/
-		
-		// __dirname = /var/task
-		console.log('IAN-TRACE [chartprocessor] - __dirname = ' + __dirname);
-
-		var dataFile = '../../tmp/' + uuid.v4() + '.json';
-
 		console.log('IAN-TRACE [chartprocessor] - dataFile = ' + dataFile);
 		
 		fs.writeFileSync(dataFile, data);
 		
-		fs.readdirSync('../../tmp/').forEach(function (name) {
-			console.log('IAN-TRACE [chartprocessor] - tmp file = ' + name);
+		fs.readdirSync(TMP_DIR).forEach(function (name) {
+			console.log('[chart-processor] - tmp file: ' + name);
 		});
-		
-		const reloadedData = fs.readFileSync(dataFile, 'utf8');
-		
-		console.log('IAN-TRACE [chartprocessor] - reloadedData = ' + reloadedData);
-		
+
 		var phantom = phantomjs.exec('phantomjs-script.js', chartTemplate, dataFile);
 
 	    	phantom.stdout.on('data', function(buf) {
 			var base64Data = String(buf).replace(/\n$/, '');
-			console.log('got base64 data: ' + base64Data);
+			console.log('[chart-processor] - got base64 data: ' + base64Data);
 			chartImageBase64 += base64Data;
 	    	});
 
