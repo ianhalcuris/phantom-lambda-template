4b5f9510bf67c85c37c54099fef1c823a97b08b4
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-08-02 14:06:29 +0100

    Update dailypatternschart.js

--- dailypatternschart.js
+++ dailypatternschart.js
@@ -11,7 +11,7 @@ exports.handler = function(event, context, callback) {
     log('handler', 'event: ' + JSON.stringify(event));
 
     // TODO use offset/range/precision from event?
-    var apiUrl = process.env.MemoBaseURL + '/memo/service/insight/hub/' + event.patientId + '/service/discreteData?precision=5';
+    var apiUrl = process.env.MemoBaseURL + '/memo/service/insight/hub/' + event.hubId + '/service/discreteData?precision=5';
     var chartTemplate = process.env.LAMBDA_TASK_ROOT + CHARTS_DIR + 'dailypatterns.html';
 	
     log('handler', 'apiUrl: ' + apiUrl);
