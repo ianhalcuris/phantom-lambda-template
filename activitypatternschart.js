b8c330928702eaeaa5c62d1d4261cc5b0fd598b1
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-08-02 12:39:21 +0100

    Update activitypatternschart.js

--- activitypatternschart.js
+++ activitypatternschart.js
@@ -9,7 +9,7 @@ exports.handler = function(event, context, callback) {
     log('handler', 'event: ' + JSON.stringify(event));
 
     // TODO use offset/range/precision from event?
-    var apiUrl = util.format(process.env.MemoBaseURL + '/memo/service/insight/patient/%s/service/trend/environment?offset=13&range=14&precision=5', event.patientId);
+    var apiUrl = process.env.MemoBaseURL + '/memo/service/insight/patient/' + event.patientId + '/service/trend/environment?offset=13&range=14&precision=5'
     var chartTemplate = process.env.LAMBDA_TASK_ROOT + '/charts/activitypatterns.html';
 	
     log('handler', 'apiUrl: ' + apiUrl);
