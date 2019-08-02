f047759b5e3ecd447b73c275bd252bf00e4c1da3
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-08-02 14:07:07 +0100

    Update bathroompatternschart.js

--- bathroompatternschart.js
+++ bathroompatternschart.js
@@ -11,7 +11,7 @@ exports.handler = function(event, context, callback) {
     log('handler', 'event: ' + JSON.stringify(event));
 
     // TODO use offset/range/precision from event?
-    var apiUrl = process.env.MemoBaseURL + '/memo/service/insight/hub/' + event.patientId + '/service/scatterData?offset=13&range=14&precision=5';
+    var apiUrl = process.env.MemoBaseURL + '/memo/service/insight/hub/' + event.hubId + '/service/scatterData?offset=13&range=14&precision=5';
     var chartTemplate = process.env.LAMBDA_TASK_ROOT + CHARTS_DIR + 'bathroompatterns.html';
 	
     log('handler', 'apiUrl: ' + apiUrl);
