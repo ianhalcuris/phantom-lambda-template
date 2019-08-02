5fd1cada09ff44547d7df9b41838f3a969755cb8
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-08-02 12:32:28 +0100

    Update activitypatternschart.js

--- activitypatternschart.js
+++ activitypatternschart.js
@@ -1,16 +1,19 @@
-var AWS = require('aws-sdk');
-var util = require('util');
 var chartProcessor = require('./chartprocessor');
+
+function log(method, message){
+    console.log('[activitypatternschart::' + method + '] - ' + message);
+}
 
 exports.handler = function(event, context, callback) {
 
-	console.log('started, event = ' + JSON.stringify(event));
-		
-	const apiUrl = util.format('https://' + process.env.API_HOSTNAME + '/memo/service/insight/patient/%s/service/trend/environment?offset=13&range=14&precision=5', event.patientId);
-	const chartTemplate = process.env.LAMBDA_TASK_ROOT + '/charts/activitypatterns.html';
+    log('handler', 'event: ' + JSON.stringify(event));
+
+    // TODO use offset/range/precision from event?
+    var apiUrl = util.format(process.env.MemoBaseURL + '/memo/service/insight/patient/%s/service/trend/environment?offset=13&range=14&precision=5', event.patientId);
+    var chartTemplate = process.env.LAMBDA_TASK_ROOT + '/charts/activitypatterns.html';
 	
-	console.log('apiUrl: ' + apiUrl);
-	console.log('chartTemplate: ' + chartTemplate);
+    log('handler', 'apiUrl: ' + apiUrl);
+    log('handler', 'chartTemplate: ' + chartTemplate);
 	
-	chartProcessor.renderChart(apiUrl, chartTemplate, context, callback);
+    chartProcessor.renderChart(apiUrl, chartTemplate, context, callback);
 };
