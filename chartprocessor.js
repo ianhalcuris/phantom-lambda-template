650e92369578684d2e8eb9061e217d5bce06d4be
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2019-08-02 10:44:17 +0100

    Update chartprocessor.js

--- chartprocessor.js
+++ chartprocessor.js
@@ -29,11 +29,49 @@ function getApiData(apiUrl) {
 	})
 }
 
+function login() {
+
+    console.log('Logging in...');
+	
+    var options = {
+        url: 'https://alcuris.eu.auth0.com/oauth/token',
+	headers: {
+  	    'Content-Type': 'application/json'
+	},
+        body: {
+	    password: 'AlCuRiS123', 
+	    audience: 'https://dev-api.memohub.co.uk', 
+	    grant_type: 'http://auth0.com/oauth/grant-type/password-realm', 
+	    scope: 'openid contacts', 
+	    realm: 'memo-insight-dev', 
+	    client_secret: '_G4ybbmZhYPR_s1mqw0YMXMBcFdQHEM3QGXe8saYhnK1cLQlSHUuCX0m7sRMsmhL', 
+	    client_id: 'AqJzLKWDlH8S6p0Jz4cWbM9QvchbeacK', 
+	    username: 'system@alcuris.co.uk'
+	}
+    };
+
+    return new Promise(function(resolve, reject) {
+	request.get(options, function(err, resp, body) {
+	    if (err) {
+		reject(err);
+	    } else {
+		resolve(body);
+	    }
+	})
+    })
+}
+
 exports.renderChart = function(apiUrl, chartTemplate, context, callback) {	
 	
 	console.log('[chart-processor] apiUrl:' + apiUrl);
 	console.log('[chart-processor] chartTemplate:' + chartTemplate);
 	
+	
+    login().then(
+        function(result) {
+
+		console.log('LOGIN RESULT: ' + result);
+		
 	getApiData(apiUrl).then(function(data) {
 
 		console.log('[chart-processor] - data: ' + data);
@@ -81,6 +119,14 @@ exports.renderChart = function(apiUrl, chartTemplate, context, callback) {
 
 		console.log('error getting discrete data: ' + err);
 		// TODO error function response
-	});
+	})
+		
+		
+	}, 
+        function(err) {
 
+		console.log('LOGIN ERROR: ' + err);
+		// TODO error function response
+	}
+    );
 };
