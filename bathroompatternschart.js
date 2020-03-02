e88266814aaed9c0aa6d1b70847a70c173f32a20
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2020-03-02 11:45:46 +0000

    Update bathroompatternschart.js

--- bathroompatternschart.js
+++ bathroompatternschart.js
@@ -1,6 +1,9 @@
 var chartProcessor = require('./chartprocessor');
 
 const CHARTS_DIR = '/charts/';
+const CHART_TEMPLATE = process.env.LAMBDA_TASK_ROOT + CHARTS_DIR + 'bathroompatterns.html';
+const API_URL_PREFIX = process.env.MemoBaseURL + '/memo/service/insight/hub/';
+const API_URL_SUFFIX = '/service/scatterData?offset=13&range=14&precision=5';
 
 function log(method, message){
     console.log('[bathroompatternschart::' + method + '] - ' + message);
@@ -8,14 +11,14 @@ function log(method, message){
 
 exports.handler = function(event, context, callback) {
 
-    log('handler', 'event: ' + JSON.stringify(event));
+//    log('handler', 'event: ' + JSON.stringify(event));
 
     // TODO use offset/range/precision from event?
-    var apiUrl = process.env.MemoBaseURL + '/memo/service/insight/hub/' + event.hubId + '/service/scatterData?offset=13&range=14&precision=5';
-    var chartTemplate = process.env.LAMBDA_TASK_ROOT + CHARTS_DIR + 'bathroompatterns.html';
+    var apiUrl = API_URL_PREFIX + event.hubId + API_URL_SUFFIX;
+    var accessToken = event.accessToken;
 	
-    log('handler', 'apiUrl: ' + apiUrl);
-    log('handler', 'chartTemplate: ' + chartTemplate);
+//    log('handler', 'apiUrl: ' + apiUrl);
+//    log('handler', 'CHART_TEMPLATE: ' + CHART_TEMPLATE);
 	
-    chartProcessor.renderChart(apiUrl, chartTemplate, context, callback);
+    chartProcessor.renderChart(apiUrl, CHART_TEMPLATE, accessToken, context, callback);
 };
