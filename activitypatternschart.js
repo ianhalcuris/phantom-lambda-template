1659d9ff6ee29410ed38ee0e14d6e6a597277f40
ianhalcuris <48080510+ianhalcuris@users.noreply.github.com>
2020-03-03 10:13:12 +0000

    Update activitypatternschart.js

--- activitypatternschart.js
+++ activitypatternschart.js
@@ -11,15 +11,23 @@ const API_URL_SUFFIX = '/service/trend/environment?offset=6&range=7&precision=5'
 exports.handler = function(event, context, callback) {
 
 //    log('handler', 'event: ' + JSON.stringify(event));
-
-    // TODO use offset/range/precision from event?
-    var apiUrl = API_URL_PREFIX + event.patientId + API_URL_SUFFIX;
 	
-    var accessToken = event.accessToken;
-//    log('handler', 'accessToken: ' + accessToken);
+    if (event["isKeepalivePing"]) {
+	var response = {
+	    statusCode: 200
+	};
+	callback(null, response);
+    } else {
+	    
+    	// TODO use offset/range/precision from event?
+    	var apiUrl = API_URL_PREFIX + event.patientId + API_URL_SUFFIX;
 	
-//    log('handler', 'apiUrl: ' + apiUrl);
-//    log('handler', 'chartTemplate: ' + chartTemplate);
+    	var accessToken = event.accessToken;
+//      log('handler', 'accessToken: ' + accessToken);
 	
-    chartProcessor.renderChart(apiUrl, CHART_TEMPLATE, accessToken, context, callback);
+//      log('handler', 'apiUrl: ' + apiUrl);
+//      log('handler', 'chartTemplate: ' + chartTemplate);
+	
+    	chartProcessor.renderChart(apiUrl, CHART_TEMPLATE, accessToken, context, callback);
+    }
 };
